ac2320c1218cca04dc5a4cf26758486757afebaf
Abdullahahmad666 <l201326@lhr.nu.edu.pk>
2024-12-02 18:26:26 +0500

    Add Firebase Registration

--- app/src/main/java/com/example/shopinglist/SignUp.java
+++ app/src/main/java/com/example/shopinglist/SignUp.java
@@ -2,17 +2,36 @@ package com.example.shopinglist;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+
 public class SignUp extends AppCompatActivity {
 
+    EditText email,etpassword;
+    Button btnSignup;
+    TextView tvLogin;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,14 +42,78 @@ public class SignUp extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Button btnsignup = findViewById(R.id.btnSignUp);
-        btnsignup.setOnClickListener(new View.OnClickListener() {
+
+        init();
+        tvLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(SignUp.this, Login.class);
+                Intent i =new Intent(SignUp.this, Login.class);
                 startActivity(i);
                 finish();
             }
         });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = email.getText().toString().trim();
+                String password = etpassword.getText().toString();
+                if(TextUtils.isEmpty(username) || TextUtils.isEmpty(password))
+                {
+                    Toast.makeText(SignUp.this, "Email or password is empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                auth.createUserWithEmailAndPassword(username, password)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                String userID = auth.getCurrentUser().getUid();
+                                // store rest of the data into firestore
+                                HashMap<String, Object> data = new HashMap<>();
+                                data.put("email",username);
+                                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                                db.collection("users")
+                                        .document(userID)
+                                        .set(data)
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if(task.isSuccessful())
+                                                {
+                                                    Toast.makeText(SignUp.this, "User Created", Toast.LENGTH_SHORT).show();
+
+                                                }
+                                                else
+                                                {
+                                                    Toast.makeText(SignUp.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
+
+                                startActivity(new Intent(SignUp.this, MainActivity.class));
+                                finish();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(SignUp.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+
+
     }
+    public void init()
+    {
+        email = findViewById(R.id.email);
+        etpassword = findViewById(R.id.password);
+        btnSignup = findViewById(R.id.btnSignUp);
+        tvLogin = findViewById(R.id.backToLogin);
+        auth = FirebaseAuth.getInstance();
+
+    }
+
 }
